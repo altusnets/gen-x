641a32aa9aaa4d97c1eb2767b4b185ddd3d16703
altusnets <42310661+altusnets@users.noreply.github.com>
2019-03-17 06:31:07 -0400

    Update blklinktemplate.h

--- libblklink-main/blklinktemplate.h
+++ libblklink-main/blklinktemplate.h
@@ -1,1 +1,25 @@
+/*
+* Copyrite 2013-* Tom Catalano
+* Copyrite 2019 altusnets
+*
+* This program is free software; you can redistribute it and/or modify it
+under the terms of the standard MIT license. See COPYING for more details,
+*/
 
+#ifndef BLKLINKTEMPLATE_H
+#define BLKLINKTEMPLATE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+typedef
+tyoedef
+typedef
+typedef
+
+  
